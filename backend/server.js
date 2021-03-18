3066f251f60bd063afec98e61a49a8d0e9651f1e
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-03-19 03:14:51 +0530

    Updated env variables + added dotenv dependancy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const express = require('express'),
 app.use(express.json())
 app.use(cors());
 
-const uri = "mongodb+srv://root:root@ingredient2recipe.uoh3o.mongodb.net/recipe-app?retryWrites=true&w=majority";
+const uri = process.env.MONGO_DB_URI;
 
 mongoose.connect(uri, {
 	useNewUrlParser: true,
