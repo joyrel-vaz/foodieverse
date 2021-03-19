2da15492d01aeb945f335f19661cead877555dd3
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-03-19 22:47:11 +0530

    Reverting to previous commit without frontend updates

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 const express = require('express'),
-      app = express(),
-      cors = require('cors'),
-      mongoose = require('mongoose'),
-      dadiKeNuske = require('./models/dadiKeNuske.js'),
-      PORT = 8080;
+app = express(),
+cors = require('cors'),
+mongoose = require('mongoose'),
+dadiKeNuske = require('./models/dadiKeNuske.js'),
+PORT = 8080;
+
+const dotenv = require('dotenv');
+const result = dotenv.config();
+if (result.error) {
+    throw result.error;
+}
 
 app.use(express.json())
 app.use(cors());
 
-const uri = process.env.MONGO_DB_URI;
-
+const uri = "mongodb+srv://root:root@ingredient2recipe.uoh3o.mongodb.net/recipe-app?retryWrites=true&w=majority";
 mongoose.connect(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
