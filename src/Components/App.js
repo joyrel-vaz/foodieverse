6f3e63610eb3d7695f21e1d493ee7a6f7f1ef0c5
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-03-22 04:09:34 +0530

    Updated Navbar - Good Design but Missing X and unrendered links

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,6 +14,7 @@ import AilmentCategory from './AilmentCategory'
 import HomeRemedies from './HomeRemedies'
 import NavigationBar from './NavBar'
 import RecipePage from './Recipe'
+import './App.css';
 
 
 function App() {
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,67 +1,89 @@
-import React from 'react'
-import {Button, Nav, Navbar, NavDropdown, Form, FormControl}  from 'react-bootstrap'
+import React, {useState, useEffect } from 'react';
+import { Button } from './Button';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import './NavBar.css'
 
 export default function NavigationBar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    return (
-        <div>
-            <>
-            <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">
-      {/*<img
-        alt="Logo"
-        src="images/logo.png"
-        className="d-inline-block align-top img-responsive"
-      />{' '}*/}
-      FOODKART
-    </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link
-                  href="/">Home</Nav.Link>
-                  <NavDropdown title="Features"
-                  id="basic-nav-dropdown">
-                    <NavDropdown.Item
-                    href="/recipes">Recipes</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="/home-remedies">Dadi Ke Nuske</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="#">Meal Planner</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="/shopping-list">Shopping List</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="#">Favourites</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="#">Settings</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="#">Foodium</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="#">Help</NavDropdown.Item>
-                    <NavDropdown.Item
-                    href="#">Recipe of the day</NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link
-                  href="#">About</Nav.Link>
-                   <Nav.Link
-                  href="#">Demo</Nav.Link>
-                   <Nav.Link
-                  href="#">Contact Us</Nav.Link>
-                  <Nav.Link className="login-button"
-                  href="/Login">Login</Nav.Link>
-                  <Nav.Link
-                  href="/recipes">Recipe Cards</Nav.Link>
-                </Nav>
-    
-                <Form inline>
-                  <FormControl type="text"
-                  placeholder="Search" className="mr-sm-2"/>
-                  <Button variant="outline-success">Search</Button>
-                  </Form>
-              </Navbar.Collapse>
-            </Navbar>
-  </>
-</div>
-    )
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+        <Router>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            FOODKART 
+            <i class='fas fa-utensils' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+               to='recipe'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Features
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/about'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/Contact'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Contact Us
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='login'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                LOGIN
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline' className='login-button'>LOGIN</Button>}
+          </Router>
+        </div>
+      </nav>
+    </>
+  );
 }
