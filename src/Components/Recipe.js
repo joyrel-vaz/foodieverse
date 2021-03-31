e73bbad2cd1993f0e808904e62b546a47916db23
Joyrel Vaz <joyrelvaz03@gmail.com>
2021-03-31 17:31:37 +0530

    replaced class by fn to display fetched recipes dynamically

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,62 +1,44 @@
-import React, { Component } from 'react'
+import React, {useState , useEffect, Component } from 'react'
 import './Recipe.css'
 import Card2 from './Card2'
 import Search from './Search'
+import {getRecipes} from '../api.js'
 
 import {Container, Row, Col} from 'react-bootstrap'
-export default class Recipes extends Component {
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            displayCategories : true,
-            displayRecipies: false,
-            recipes: ""
-        }
-    }
 
-    render() {
+export default function Recipes () {
+    const [recipes, setRecipes] = useState([]);
+    const fetchRecipes=async()=>{
+        try{
+          const recipes = await getRecipes();
+          setRecipes(recipes);
+          console.log(recipes);
+        }catch(error){
+          console.log(error);
+        }  
+      }
+      
+      useEffect(() => {
+        fetchRecipes();
+      },[]);
+
+
         return (
             <>
             <br></br><br></br>
             <Search></Search>
             <Container fluid>
                 <Row>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                </Row>
-                <Row>
-                <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                
-                </Row>
-                <Row>
-                <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                
-                </Row>
-                <Row>
-                <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                
-                </Row>
-                <Row>
-                <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                    <Col><Card2></Card2></Col>
-                
+                    {
+                    recipes.map(r =>
+                    <Col><Card2 
+                    title = {r["Recipe Title"]}
+                    instructions = {r["Instructions"]}
+                    ingredients = {r["Ingredients"]}
+                    image = {r["Images"]}          
+                    ></Card2></Col>)}
                 </Row>
                 </Container>
             </>
         )
-    }
 }
