49d17f1ed0f8ab6d63a6e184d0e6436780682d58
Joyrel Vaz <joyrelvaz03@gmail.com>
2021-04-01 15:38:19 +0530

    added bottom margin

--- src/Components/Card2.js
+++ src/Components/Card2.js
@@ -21,6 +21,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 300,
+    marginBottom: 20
   },
   media: {
     height: 0,
@@ -97,23 +98,10 @@ export default function RecipeReviewCard(props) {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography paragraph>Method:</Typography>
-          {/*<Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>*/}
           <Typography paragraph>
             {instructions}
           </Typography>
-         { /*<Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-         </Typography> 
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>*/}
+
         </CardContent>
       </Collapse>
     </Card>
