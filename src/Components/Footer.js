6590017b07675e02e6e7066a4244747a3b7ee2fd
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-04-04 23:24:51 +0530

    Fixed SignUp, ForgetPassword and Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,30 +8,19 @@ export default function Footer() {
         <div className="main-footer">
         <div className="container">
         <div className="row">
-        <div className="col-md-4 col-sm-8">
-        <br></br><br></br><br></br>
+        <div className="col-md-8 col-sm-18">
+        <br></br><br></br>
         <Navbar.Brand className="nav-footer" href="/">
             FOODKART
         </Navbar.Brand>
         </div>
-        <div className="col-md-4 col-sm-8">
+        <div className="col-md-4 col-sm-6">
         <br></br>
-            <h6>Food For Foodies</h6>
-            Crave and you have it! Who needs a recipe book when you have FoodKart!
-            <ul >
-                <li>Recipe By Ingredient</li>
-                <li>Recipe Search</li>
+            <ul class="footer-list">
+                <li >Recipe By Ingredient</li>
+                <li>Recipe by Name</li>
                 <li>Daadi Ke Nuskhe</li>
             </ul>            
-        </div>
-        <div className="col-md-4 col-sm-8">
-        <br></br>
-            <h6>Wanna Get in Touch?</h6>
-            <ul className="list-unstyled">
-                <li>Email</li>
-                <li>Facebook</li>
-                <li>Twitter</li>
-            </ul>
         </div>
         </div>
         <div className="footer-bottom">
