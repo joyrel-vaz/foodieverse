d8632f17d9b6752e5e885ae42b08f497797b67ed
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-04-06 17:12:26 +0530

    Edited Navbar + Landing Page

--- src/Components/ControlledCarousel.js
+++ src/Components/ControlledCarousel.js
@@ -14,40 +14,23 @@ export default function ControlledCarousel() {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={'images/ingredients.jpg'}
+            src={'images/ingred1.png'}
             alt="First slide"
           />
-          
-          <Carousel.Caption>
-            <h3>Your Ingredients, Our Recipe</h3>
-            <p>Immerse yourselves into the world of innovative cuisine.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={'images/cuisine.jpg'}
+            src={'images/ingred2.png'}
             alt="Second slide"
           />
-  
-          <Carousel.Caption>
-            <h3>For Cravings that we Satsify</h3>
-            <p>Name it and you Have it!</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={'images/remedies.jpg'}
+            src={'images/ingred2.png'}
             alt="Third slide"
           />
-  
-          <Carousel.Caption>
-            <h3>Medicinal Remedies at your Fingertips</h3>
-            <p>
-            Missing home and your Granny's homemade remedies? We have got you covered.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     );
