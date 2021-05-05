e7c45ba304fb67fcee8cf2879717fe82bf43fd9a
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-05-05 13:49:55 +0530

    Updated Surprise.js

--- src/Components/Surprise.js
+++ src/Components/Surprise.js
@@ -1,16 +1,106 @@
 import React from 'react'
 import { Container} from 'react-bootstrap';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import RedeemIcon from '@material-ui/icons/Redeem';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  buttonSuccess: {
+    backgroundColor: '#C90F03',
+    '&:hover': {
+      backgroundColor: '#C90F03',
+    },
+  },
+  fabProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 export default function Surprise() {
+  const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const timer = React.useRef();
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
+
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+      timer.current = window.setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 2000);
+    }
+  };
+
   return (
       <>
-        <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh", padding:'0 0 0 0'}}
+       <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh", padding:'0 0 0 0'}}
+      >
+      <center>
+            <div className="w-100" style={{maxWidth:"400px" , padding:"20px"}}>
+        <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Fab
+          aria-label="save"
+          className={buttonClassname}
+          onClick={handleButtonClick}
         >
-              <div className="w-100" style={{maxWidth:"400px" , padding:"20px"}}>
-          <center><h4>BIG SURPRISE COMING SOON!</h4>
-        </center>
-        </div>
-        </Container>
+          {success ? <RedeemIcon /> : <SentimentVerySatisfiedIcon />}
+        </Fab>
+        {loading && <CircularProgress size={68} className={classes.fabProgress} />}
+      </div>
+      <div className={classes.wrapper}>
+        <Button
+          variant="contained"
+          className={buttonClassname}
+          disabled={loading}
+          onClick={handleButtonClick}
+        >
+         Get Surprised!!!
+        </Button>
+        {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+      </div>
+    </div>
+    Enter Recipe Card here!
+    </div>
+    </center>
+    </Container>
     </>
   );
 }
