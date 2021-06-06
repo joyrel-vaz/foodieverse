e68df7dfb6f91231b1fad8e6bf4b84e58e967b32
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-06-06 20:07:17 +0530

    Updated Integration page

--- src/Components/IntegratedCard.js
+++ src/Components/IntegratedCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Switch from '@material-ui/core/Switch';
-import TwitterIcon from '@material-ui/icons/Twitter';
-
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'block',
-    marginTop:'2rem'
+    marginTop:'2rem',
+  },
+  root1:{
+    display:'flex',
+    marginTop:'1rem',
   },
   details: {
     display: 'flex',
@@ -20,9 +24,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MediaControlCard() {
+export default function ImageAvatars() {
   const classes = useStyles();
-
   const [state, setState] = React.useState({
     checkedA: true,
     checkedB: true,
@@ -35,13 +38,17 @@ export default function MediaControlCard() {
     <Card className={classes.root}>
       <div className={classes.details}>
         <CardContent className={classes.content}>
-        <TwitterIcon/> Twitter
-        <Switch className="switch-integration"
+        <InstagramIcon/> Instagram
+      <Switch className="switch-integration"
         checked={state.checkedA}
         onChange={handleChange}
         name="checkedA"
         inputProps={{ 'aria-label': 'secondary checkbox' }}
         />
+        <div className={classes.root1}>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <h6>Username: mister.foodie</h6>
+        </div>
         </CardContent>
         
       </div>
--- src/Components/Integration.js
+++ src/Components/Integration.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import IntegrationCard from './IntegrationCard.js'
+import IntegratedCard from './IntegratedCard.js'
 import { Container} from 'react-bootstrap';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,9 +20,10 @@ export default function ListDividers() {
   return (
       <><Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh", padding:'0 0 0 0'}}
       >
-            <div className="w-100" style={{maxWidth:"400px" , padding:"20px"}}>
-        <center><h4>INTEGRATIONS</h4></center>
-      <IntegrationCard/><IntegrationCard />
+            <div className="w-100 div-main" style={{maxWidth:"400px" , padding:"20px"}}>
+        <center><h4>INTEGRATIONS<AddCircleOutlineIcon className="switch-integration"/></h4></center>
+        
+      <IntegratedCard/><IntegrationCard />
       </div>
       </Container>
     </>
