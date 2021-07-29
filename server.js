ff990b23f5fcae15cb56b7b4970329f0824d9256
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-07-29 16:44:54 +0530

    deployment app version 1.0.0

--- server.js
+++ server.js
@@ -842,5 +842,16 @@ app.get('/api/ingredients' , (req,res)=>{
     })
 })
 
+
+//serve static assets if in production
+if(process.env.NODE_ENV === "production"){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname , 'client','build' , 'index.html'));
+    });
+}
+
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
 
