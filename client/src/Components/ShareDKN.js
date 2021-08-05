ed89f124fcec4f5c61f20f992336f95dab17ae09
Joyrel Vaz <joyrelvaz03@gmail.com>
2021-08-06 02:15:09 +0530

    Dadi ke nushke share [from hosted]

--- client/src/Components/ShareDKN.js
+++ client/src/Components/ShareDKN.js
@@ -12,12 +12,8 @@ const ShareDKN=({title, desc})=>{
     <>
         <WhatsappShareButton className="social-media-icon social-margin" url={`Check out this remedy of ${title}: ${desc}`}>
         <WhatsappIcon size={32} round={true}/></WhatsappShareButton>
-        <FacebookShareButton className="social-media-icon social-margin" quote={`Check out this remedy of ${title}:${desc}`}>
-        <FacebookIcon size={32} round={true}/></FacebookShareButton>
         <EmailShareButton className="social-media-icon social-margin" subject={title} body={`Check out this remedy of ${title}:${desc}`}>
         <EmailIcon size={32} round={true}/></EmailShareButton>
-        <LinkedinShareButton className="social-media-icon social-margin" title={`Check out this remedy of ${title}:${desc} `}>
-        <LinkedinIcon size={32} round={true}/></LinkedinShareButton>
         <TwitterShareButton className="social-media-icon social-margin" url={`Check out this remedy of ${title}:${desc}`}>
         <TwitterIcon size={32} round={true}/></TwitterShareButton>
     </>
